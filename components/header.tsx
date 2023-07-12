180e62047356928b3a936e7b182e646440830eb0
willblair0708 <85643015+willblair0708@users.noreply.github.com>
2023-07-12 14:09:03 -0400

    Update layout

--- components/header.tsx
+++ components/header.tsx
@@ -9,7 +9,6 @@ import { Sidebar } from '@/components/sidebar'
 import { SidebarList } from '@/components/sidebar-list'
 import {
   IconGitHub,
-  IconOlympiad,
   IconNextChat,
   IconSeparator,
   IconVercel
@@ -37,8 +36,8 @@ export async function Header() {
           </Sidebar>
         ) : (
           <Link href="/" target="_blank" rel="nofollow">
-            <IconOlympiad className="w-6 h-6 mr-2 dark:hidden" />
-            <IconOlympiad className="hidden w-6 h-6 mr-2 dark:block" />
+            <IconNextChat className="w-6 h-6 mr-2 dark:hidden" inverted />
+            <IconNextChat className="hidden w-6 h-6 mr-2 dark:block" />
           </Link>
         )}
         <div className="flex items-center">
@@ -55,20 +54,20 @@ export async function Header() {
       <div className="flex items-center justify-end space-x-2">
         <a
           target="_blank"
-          href="https://science-olympiad.webflow.io/"
+          href="https://github.com/thorwebdev/vercel-ai-chatbot"
           rel="noopener noreferrer"
           className={cn(buttonVariants({ variant: 'outline' }))}
         >
           <IconGitHub />
-          <span className="hidden ml-2 md:flex">Documentation</span>
+          <span className="hidden ml-2 md:flex">GitHub</span>
         </a>
         <a
-          href="https://science-olympiad.webflow.io/"
+          href="https://github.com/thorwebdev/vercel-ai-chatbot"
           target="_blank"
           className={cn(buttonVariants())}
         >
-          <IconOlympiad className="mr-2" />
-          <span className="hidden sm:block">Science Olympiad</span>
+          <IconVercel className="mr-2" />
+          <span className="hidden sm:block">Deploy to Vercel</span>
           <span className="sm:hidden">Deploy</span>
         </a>
       </div>
